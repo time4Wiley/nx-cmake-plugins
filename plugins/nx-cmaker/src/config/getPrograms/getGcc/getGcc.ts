09d1761ea33a0b17ca9a5a19999565ed4df3e251
Wei Sun <sunwei415@126.com>
2024-09-18 11:35:42 +0800

    Revert "fix(getGcc): update Darwin GCC path to use CXX_ON_DARWIN constant"
    This reverts commit 82384fbbbd0e9622f8b2c86b9da128d13da640be.

--- plugins/nx-cmaker/src/config/getPrograms/getGcc/getGcc.ts
+++ plugins/nx-cmaker/src/config/getPrograms/getGcc/getGcc.ts
@@ -2,16 +2,15 @@ import { getProgram } from '../getProgram/getProgram';
 import { GCC } from '../getPrograms';
 import { isDarwin } from '@/util';
 
-export const CXX_ON_DARWIN = '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/c++ -std=c++14';
-
 export const getGcc = () => {
     if (isDarwin(process.platform)) {
-        return CXX_ON_DARWIN;
+        return '/opt/homebrew/bin/gcc-14';
     }
     const gcc = getProgram(GCC);
     return gcc;
 };
 
+export const CXX_ON_DARWIN = '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/c++';
 export const CC_ON_DARWIN = '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc';
 
 export const getCc = () => {
